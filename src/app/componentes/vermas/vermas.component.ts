f2300522396b8bd8990c89389d57e56f39a9262f
Erikagam <68310719+Erikagam@users.noreply.github.com>
2021-02-04 02:11:26 -0600

    "modificacion vista cel 3 primeras aseguradoras"

--- src/app/componentes/vermas/vermas.component.ts
+++ src/app/componentes/vermas/vermas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-vermas',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vermas.component.css']
 })
 export class VermasComponent implements OnInit {
-
+  verCarousle: boolean = false;
+  resizeObservable$: Observable<Event>
+  resizeSubscription$: Subscription
   constructor() { }
 
   ngOnInit(): void {
+    this.resizeObservable$ = fromEvent(window, 'resize')
+    this.resizeSubscription$ = this.resizeObservable$.subscribe( event => {
+      if(event.currentTarget.outerWidth <= 576){
+        this.verCarousle = true
+      } else {
+        this.verCarousle = false
+      }
+    })
   }
 
+  ngOnDestroy() {
+    this.resizeSubscription$.unsubscribe()
+}
 }
