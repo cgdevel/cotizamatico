6c74f9bf9aa012bd0d89d8e0f9d522de9eec331f
Erikagam <68310719+Erikagam@users.noreply.github.com>
2021-02-04 04:57:38 -0600

    "cambio en condicion resize"

--- src/app/componentes/vermas/vermas.component.ts
+++ src/app/componentes/vermas/vermas.component.ts
@@ -7,7 +7,7 @@ import { fromEvent, Observable, Subscription } from 'rxjs';
   styleUrls: ['./vermas.component.css']
 })
 export class VermasComponent implements OnInit {
-  verCarousle: boolean = false;
+  verCarousle: boolean=false;
   resizeObservable$: Observable<Event>
   resizeSubscription$: Subscription
   constructor() { }
@@ -16,7 +16,7 @@ export class VermasComponent implements OnInit {
     this.resizeObservable$ = fromEvent(window, 'resize')
     this.resizeSubscription$ = this.resizeObservable$.subscribe( event => {
       console.log((event.target as Window).innerWidth)
-      if((event.target as Window).innerWidth <= 576){
+      if((event.target as Window).innerWidth <1216){
         this.verCarousle = true
       } else {
         this.verCarousle = false
