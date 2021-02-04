bd369f4e79ecc57b02e70ee0585103e828b08e18
Erikagam <68310719+Erikagam@users.noreply.github.com>
2021-02-04 05:38:01 -0600

    "checar detalles "

--- src/app/componentes/vermas/vermas.component.ts
+++ src/app/componentes/vermas/vermas.component.ts
@@ -7,13 +7,14 @@ import { fromEvent, Observable, Subscription } from 'rxjs';
   styleUrls: ['./vermas.component.css']
 })
 export class VermasComponent implements OnInit {
-  verCarousle: boolean=false;
+  verCarousle: boolean;
   resizeObservable$: Observable<Event>
   resizeSubscription$: Subscription
   wiii: number;
   constructor() { }
 
   ngOnInit(): void {
+    
     this.resizeObservable$ = fromEvent(window, 'resize')
     this.resizeSubscription$ = this.resizeObservable$.subscribe( event => {
       console.log((event.target as Window).innerWidth)
@@ -24,27 +25,6 @@ export class VermasComponent implements OnInit {
         this.verCarousle = false
       }
     })
-    var getBrowserInfo = function() {
-      var ua= navigator.userAgent, tem, 
-      M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-      if(/trident/i.test(M[1])){
-          tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
-          return 'IE '+(tem[1] || '');
-      }
-      if(M[1]=== 'Chrome' && this.wiii<1216 ){
-          this.verCarousle = true
-          tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
-          if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
-      }
-      M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-      if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
-      return M.join(' ');
-  };
-  
-  // console.log(getBrowserInfo());
-  }
-
-  ngOnDestroy() {
-    this.resizeSubscription$.unsubscribe()
+ 
 }
 }
