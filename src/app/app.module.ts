f99cf9fbd8e8067a8709e9ba4ef9f92764f97370
cgdevel <52430823+cgdevel@users.noreply.github.com>
2021-02-12 17:39:27 -0600

    Actualización en app.component y layout
    Actualizaciónes:
    - app.component:
        + Actualización para el layout
        + Cambio de titulo de la aplicación
    - app.module
       + Formato de código
       + Corrección en un de error de Bootstrap
    
    - Actualización de Layout
    
    - index.html
       + Cambio de nombre de la aplicación
       + Ajuste para que el header y el footer se encuentren en el layout
    
    - style.css
       + ajuste en el css principal para que el tipo de letra de los tooltip y las opciones de los combos sean Exo 2
       + Aplicación de estilo para el combo (separar del default.theme)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {LayoutComponent} from './layout/layout.component'
+import { LayoutComponent } from './layout/layout.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes//header/header.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { Pagina1Component } from './componentes/pagina1/pagina1.component';
 import { InfoaseguradoComponent } from './componentes/pagina1/infoasegurado/infoasegurado.component';
-import {InfovehiculoComponent} from './componentes/pagina1/infovehiculo/infovehiculo.component';
-import {Pagina2Component } from './componentes/pagina2/pagina2.component'
+import { InfovehiculoComponent } from './componentes/pagina1/infovehiculo/infovehiculo.component';
+import { Pagina2Component } from './componentes/pagina2/pagina2.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { from } from 'rxjs';
-import { NgbModule, NgbPaginationModule,NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbModule,
+  NgbPaginationModule,
+  NgbAlertModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { VermasComponent } from './componentes/vermas/vermas.component';
-import {Pagina3Component } from './componentes/pagina3/pagina3.component'
+import { Pagina3Component } from './componentes/pagina3/pagina3.component';
 
 @NgModule({
-  declarations: [AppComponent,
-  HeaderComponent,
-  FooterComponent,
-  Pagina1Component,
-  InfoaseguradoComponent,
-  InfovehiculoComponent,
-  Pagina2Component,
-  Pagina3Component,
-  LayoutComponent,
-  VermasComponent
-],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    Pagina1Component,
+    InfoaseguradoComponent,
+    InfovehiculoComponent,
+    Pagina2Component,
+    Pagina3Component,
+    LayoutComponent,
+    VermasComponent,
+  ],
   imports: [
     FormsModule,
     BrowserModule,
     AppRoutingModule,
-     HttpClientModule,
-     NgSelectModule,
-     NgbModule,
-     NgbPaginationModule, 
-     NgbAlertModule
+    HttpClientModule,
+    NgSelectModule,
+    NgbModule,
+    NgbPaginationModule,
+    NgbAlertModule,
   ],
   providers: [],
-  bootstrap: [AppComponent,HeaderComponent,FooterComponent,InfovehiculoComponent,InfoaseguradoComponent,Pagina1Component,Pagina2Component,Pagina3Component, VermasComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
