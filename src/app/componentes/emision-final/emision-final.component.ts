3a113b5036df0c2bc68b3157259675cef83afbf6
David Herrera <mudakmudaka@gmail.com>
2021-03-18 20:54:04 -0600

    added emision-final coomponent

---
+++ src/app/componentes/emision-final/emision-final.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emision-final',
+  templateUrl: './emision-final.component.html',
+  styleUrls: ['./emision-final.component.css'],
+})
+export class EmisionFinalComponent implements OnInit {
+  success = true;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
